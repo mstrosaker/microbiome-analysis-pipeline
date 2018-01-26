5acc2ec2086705cd826a86f30ad3e909210612f3
Mike Strosaker <mstrosaker@gmail.com>
2018-01-26 09:46:31 -0600

    add script for processing FASTQ files into quality-checked FASTA files

--- README.rst
+++ README.rst
@@ -16,28 +16,90 @@ This pipline was used with:
 
 Additional prerequisites include:
 
+- PEAR or VSEARCH (for joining paired-end reads)
 - uclust
-- muscle (multiple alignment)
+- usearch
+- muscle (multiple alignment, ITS sequences only)
 
 Process
 -------
 
-(This section will be refined as the scripts are committed to the repository.)
-
-The directory structure for you project should look something like this:
+The directory structure for your project should look something like this:
 
 ::
 
   project_name
    |- sample_name1
-   |   |- fastq
+   |   |- fastq_16S
+   |   |- fastq_ITS
    |- sample_name2
-       |- fastq
+   |   |- fastq_16S
+   |   |- fastq_ITS
+   |- sample_name3
+       |- fastq_16S
 
-The fastq directories should contain the forward and reverse reads in FASTQ
-format, for both 16S and ITS sequencing.
+The fastq_16S and fastq_ITS directories should contain the forward and
+reverse reads in FASTQ format, for sequencing with 16S and ITS2 barcodes
+respectively.  This pipeline assumes that barcodes have already been removed.
+The fastq_16S or fastq_ITS directories should not be created if the appropriate
+data is not available for the sample (as in the case of sample_name3 above).
 
-#. Joining paired-end reads (script to be committed)
-#. Performing OTU picking to generate OTU tables (script to be committed)
-#. Run ``analyze_otu_table.sh`` to perform analysis on the generated OTU tables
+The directories for the samples must match the sample names in the metadata
+file.  All of the scripts in this repository should be run from the base
+(``project_name``) directory.
+
+** Step 1: Sample Preparation **
+
+Running ``prepare_samples.sh`` will generate FASTA files from the FASTQ files
+in the sample directories.  It uses PIPITS to reindex the reads, join the
+paired-end reads (using PEAR or VSEARCH, depending on the version of PIPITS),
+do quality filtering (using FASTX), and convert to FASTA.  QIIME is then used
+to ensure that the sequences are named appropriately in the FASTA files.
+
+A ``fasta`` directory will be created in each sample directory with the 16S and
+ITS FASTA files.  A ``prepared_fasta_<date>`` directory will be created, which
+contains a FASTA file that incorporates all 16S sequences from all samples,
+and one that incorporates all ITS sequences.  All FASTA files are gzipped.
+The ``scripts`` subdirectory includes the data necessary to reproduce the run,
+a log file that contains the script output, and a stats file that lists the
+number of sequences in the FASTA file(s) for each sample.
+
+This script should be run as follows:
+
+``bash prepare_samples.sh <project_name> <metadata_file_name>``
+
+** Step 2: OTU Picking **
+
+(script to be committed)
+
+Uses QIIME for 16S, and PIPITS for ITS2.
+
+- Chimeric sequences are identified and removed.
+- OTU picking is run, and an OTU table is created in the biom format.
+- A representative set is selected.
+- Taxonomy is assigned and added to the OTU table.
+
+** Step 3: OTU Table Analysis **
+
+Running ``analyze_otu_table.sh`` performs analysis on the generated OTU tables
+using the tools provided by QIIME.
+
+- A phylogeny tree is created for the OTUs.
+- Alpha diversity is calculated for each sample, using rarefaction, and the
+  specified groups are compared.
+- Beta diversity is analyzed, through jackknifing and through plots.
+- Plots are generated to summarize the taxa.
+- Group significance is calculated.
+
+This script should be run as follows:
+
+``bash analyze_otu_table.sh <project_name> <16S|ITS> <metadata_file_name> <max_rarefraction_depth> <metadata_columns>``
+
+It should be run twice, once for 16S and once for ITS.  The metadata_columns
+argument should be a comma separated list of column names from the metadata
+file that define groups for comparison, such as ``TreatmentState,Description``.
+To determine the value for rarefraction depth, run
+``biom summarize-table -i <biom_file>``, note the Min value (the smallest
+number of sequences in a sample), and choose a number somewhat smaller than
+that value.
 
---
+++ prepare_samples.sh
@@ -0,0 +1,174 @@
+#!/bin/bash
+
+# Run "source activate qiime1" prior to running this script
+#
+# This script takes two arguments:
+#    - the project name
+#    - metadata filename
+# The result is a single FASTA file containing the sequences from all the
+# samples listed in the metadata file, joined, quality filtered, and
+# named appropriately for the following steps in the pipeline.
+
+if ! which align_seqs.py > /dev/null 2>&1; then
+    echo
+    echo "Run 'source activate qiime1' first."
+    echo
+    exit 1;
+fi
+
+if [[ $# -ne 2 ]]; then
+    echo
+    echo "This script requires two arguments:"
+    echo "  - the project name"
+    echo "  - the filename of the metadata file"
+    echo
+    exit 1;
+fi
+
+project=$1
+metadata_file=$2
+script=`basename $0`
+script_base=${script%.*}
+
+date=$(date +%Y%m%d)
+
+results_dir=prepared_fasta_${date}
+
+if [[ -e ${results_dir} ]]; then
+    echo
+    echo "Output directory (${results_dir}) already exists."
+    echo
+    exit 1;
+fi
+echo "Results will be written to ${results_dir}"
+
+# create results directory and copy this script to it, in case we need to
+# refer to it later
+mkdir ${results_dir}
+mkdir ${results_dir}/scripts
+cp "$(readlink -f $0)" "${results_dir}/scripts"
+
+# save the arguments that this was invoked with to the results directory
+this_pid=${BASHPID}
+cat /proc/${this_pid}/cmdline | tr "\0" " " > ${results_dir}/scripts/${script_base}_cmdline.txt
+
+# output to both the console and a log file in the results directory
+log_file=${results_dir}/scripts/${script_base}.log
+echo "Logging to ${log_file}"
+exec > >(tee -a ${log_file} )
+exec 2> >(tee -a ${log_file} >&2)
+
+# save the specified metadata file
+echo "Using metadata from ${metadata_file}"
+cp ${metadata_file} ${results_dir}/scripts/${script_base}_metadata.txt
+metadata_file=${results_dir}/scripts/${script_base}_metadata.txt
+
+# create a file to store sample statistics
+stats_file=${results_dir}/scripts/${project}_stats.txt
+echo "sample	16S_seqs	ITS_seqs" > ${stats_file}
+
+# read in the sample names from the metadata file
+samples=( $(awk '{print $1}' ${metadata_file}) )
+samples=("${samples[@]:1}")	# remove the column header (first entry)
+
+echo "${#samples[@]} samples listed in metadata file"
+echo "samples: ${samples[@]}"
+echo
+
+samples_16S=()
+samples_ITS=()
+fasta_16S=()
+fasta_ITS=()
+
+for sample in "${samples[@]}"
+do
+    if [[ ! -d "${sample}" ]]; then
+        echo "No directory for sample ${sample}"
+    else
+        cd $sample
+        num_16S_seqs="NA"
+        num_ITS_seqs="NA"
+
+        for seq_type in 16S ITS; do
+
+            if [[ -d "fastq_${seq_type}" ]]; then
+
+                echo "Processing sample ${sample} ${seq_type}"
+
+                # paired end joining and quality filtering
+                pipits_getreadpairslist -i fastq_${seq_type} -o list_file
+                pipits_prep -i fastq_${seq_type} -o pipits_prep_output -l list_file
+
+                if [[ ! -d "fasta" ]]; then
+                    mkdir fasta
+                fi
+
+                mv pipits_prep_output/prepped.fasta fasta/${sample}_${seq_type}.fasta
+                rm -rf pipits_prep_output
+                rm list_file
+
+                # add sample labels to each sequence in FASTA file
+                cd fasta
+                cat > mfile.txt << EOF
+#SampleID	BarcodeSequence	LinkerPrimerSequence	InputFileName	Description
+${sample}			${sample}_${seq_type}.fasta	none
+EOF
+
+                add_qiime_labels.py -m mfile.txt -i . -c InputFileName -o update
+                mv update/combined_seqs.fna ./${sample}_${seq_type}.fasta
+                rm -rf update
+                rm mfile.txt
+
+                if [[ $seq_type == "16S" ]]; then
+                    samples_16S+=("$sample")
+                    num_16S_seqs=$(grep "^>" ${sample}_${seq_type}.fasta | wc -l)
+                    fasta_16S+=("${sample}/fasta/${sample}_${seq_type}.fasta")
+                    echo "${num_16S_seqs} sequences"
+                else
+                    samples_ITS+=("$sample")
+                    num_ITS_seqs=$(grep "^>" ${sample}_${seq_type}.fasta | wc -l)
+                    fasta_ITS+=("${sample}/fasta/${sample}_${seq_type}.fasta")
+                    echo "${num_ITS_seqs} sequences"
+                fi
+
+                cd ..
+
+            fi
+
+        done
+
+        cd ..
+        echo "${sample}	${num_16S_seqs}	${num_ITS_seqs}" >> ${stats_file}
+    fi
+done
+echo
+
+# concatenate the individual sample FASTA files into single, large FASTA files
+if [[ ${#fasta_16S[@]} -gt 0 ]]; then
+    cat ${fasta_16S[@]} > ${results_dir}/${project}_16S.fasta
+    nseqs=$(grep "^>" ${results_dir}/${project}_16S.fasta | wc -l)
+    gzip ${results_dir}/${project}_16S.fasta
+fi
+echo "${#samples_16S[@]} samples with 16S data"
+echo "samples: ${samples_16S[@]}"
+echo "${nseqs} 16S sequences total in ${results_dir}/${project}_16S.fasta"
+
+if [[ ${#fasta_ITS[@]} -gt 0 ]]; then
+    cat ${fasta_ITS[@]} > ${results_dir}/${project}_ITS.fasta
+    nseqs=$(grep "^>" ${results_dir}/${project}_ITS.fasta | wc -l)
+    gzip ${results_dir}/${project}_ITS.fasta
+fi
+echo "${#samples_ITS[@]} samples with ITS data"
+echo "samples: ${samples_ITS[@]}"
+echo "${nseqs} ITS sequences total in ${results_dir}/${project}_ITS.fasta"
+
+# go through and gzip all the FASTA files for the individual samples
+for sample in "${samples[@]}"
+do
+    if [[ -d "${sample}/fasta" ]]; then
+        cd ${sample}/fasta
+        gzip *
+        cd ../..
+    fi
+done
+
